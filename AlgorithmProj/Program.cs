f50b088be3da14946b94ea47b8031c3586084539
Behruzbek Jamilov <behruzbekjamilov5@gmail.com>
2023-01-28 16:32:41 +0500

    clear

--- AlgorithmProj/Program.cs
+++ AlgorithmProj/Program.cs
@@ -1,101 +1,101 @@
 ﻿Behruz:
-//1-misol
-//int number = int.Parse(Console.ReadLine());
-//if(number>0)
-//{
-//Console.WriteLine(++number);
-//}
-//else
-//{
-//Console.WriteLine(number);
-//}
-//2-misol
+//1 - misol
 //int number = int.Parse(Console.ReadLine());
 //if (number > 0)
 //{
-//Console.WriteLine(++number);  
+//    Console.WriteLine(++number);
 //}
 //else
 //{
-//Console.WriteLine(number - 2);
+//    Console.WriteLine(number);
 //}
-//3-misol
+//2 - misol
 //int number = int.Parse(Console.ReadLine());
 //if (number > 0)
 //{
-//Console.WriteLine(++number);  
+//    Console.WriteLine(++number);
+//}
+//else
+//{
+//    Console.WriteLine(number - 2);
+//}
+//3 - misol
+//int number = int.Parse(Console.ReadLine());
+//if (number > 0)
+//{
+//    Console.WriteLine(++number);
 //}
 //else if (number < 0)
 //{
-//Console.WriteLine(number - 2);
+//    Console.WriteLine(number - 2);
 //}
 //else
 //{
-//number = 10;
-//Console.WriteLine(number);
+//    number = 10;
+//    Console.WriteLine(number);
 //}
-//4-misol
+//4 - misol
 //int number = int.Parse(Console.ReadLine());
 //int number1 = int.Parse(Console.ReadLine());
 //int number2 = int.Parse(Console.ReadLine());
 //if ((number > 0 && number1 < 0 && number2 < 0) || (number < 0 && number1 > 0 && number2 < 0) || (number < 0 && number1 < 0 && number2 > 0))
 //{
-//Console.WriteLine(" Musbat sonlar soni " + 1 + " ta");
+//    Console.WriteLine(" Musbat sonlar soni " + 1 + " ta");
 //}
 //else if ((number > 0 && number1 > 0 && number2 < 0) || (number > 0 && number1 < 0 && number2 > 0) || (number < 0 && number1 > 0 && number2 > 0))
 //{
-//Console.WriteLine(" Musbat sonlar soni " + 2 + " ta");
+//    Console.WriteLine(" Musbat sonlar soni " + 2 + " ta");
 //}
 //lse if (number > 0 && number1 > 0 && number2 > 0)
 //{
-//Console.WriteLine(" Musbat sonlar soni " + 3 + " ta ");
+//    Console.WriteLine(" Musbat sonlar soni " + 3 + " ta ");
 //}
-//5-misol
+//5 - misol
 //int number = int.Parse(Console.ReadLine());
 //int number1 = int.Parse(Console.ReadLine());
 //int number2 = int.Parse(Console.ReadLine());
-//if ((number > 0 && number1 < 0 && number2 < 0)||(number < 0 && number1 > 0 && number2 < 0)||(number < 0 && number1 < 0 && number2 > 0))
+//if ((number > 0 && number1 < 0 && number2 < 0) || (number < 0 && number1 > 0 && number2 < 0) || (number < 0 && number1 < 0 && number2 > 0))
 //{
-//Console.WriteLine(" Musbat sonla soni " + 1 + " ta");
-//Console.WriteLine(" Manfiy sonlar soni " + 2 + " ta");
+//    Console.WriteLine(" Musbat sonla soni " + 1 + " ta");
+//    Console.WriteLine(" Manfiy sonlar soni " + 2 + " ta");
 //}
-//else if ((number > 0 && number1 > 0 && number2 < 0)||(number < 0 && number1 > 0 && number2 > 0)||(number > 0 && number1 < 0 && number2 > 0))
+//else if ((number > 0 && number1 > 0 && number2 < 0) || (number < 0 && number1 > 0 && number2 > 0) || (number > 0 && number1 < 0 && number2 > 0))
 //{
-//Console.WriteLine(" Musbat sonlar soni " + 2 + " ta");
-//Console.WriteLine(" Manfiy sonlar soni " + 1 + " ta");
+//    Console.WriteLine(" Musbat sonlar soni " + 2 + " ta");
+//    Console.WriteLine(" Manfiy sonlar soni " + 1 + " ta");
 //}
 //else if ((number > 0 && number1 > 0 && number2 > 0))
 //{
-//Console.WriteLine(" Musbat sonlar soni " + 3 + " ta");
-//Console.WriteLine(" Manfiy sonlar soni " + 0 + " ta");
+//    Console.WriteLine(" Musbat sonlar soni " + 3 + " ta");
+//    Console.WriteLine(" Manfiy sonlar soni " + 0 + " ta");
 //}
 //else
 //{
-//Console.WriteLine(" Manfiy sonlar soni " + 3 + " ta");
+//    Console.WriteLine(" Manfiy sonlar soni " + 3 + " ta");
 //}
-//6-misol
+//6 - misol
 //int A = int.Parse(Console.ReadLine());
 //int B = int.Parse(Console.ReadLine());
 //if (A > B)
 //{
-//Console.WriteLine( A );
+//    Console.WriteLine(A);
 //}
 //else
 //{
-//Console.WriteLine( B );
+//    Console.WriteLine(B);
 //}
-//7-misol
+//7 - misol
 //int A = int.Parse(Console.ReadLine());
 //int B = int.Parse(Console.ReadLine());
 //if (A < B)
 //{
-//Console.WriteLine( A );
+//    Console.WriteLine(A);
 //}
 //else
 //{
-//Console.WriteLine( B );
+//    Console.WriteLine(B);
 //}
-//8-misol
+//8 - misol
 //int A = int.Parse(Console.ReadLine());
 //int B = int.Parse(Console.ReadLine());
 //if (A > B)
@@ -108,7 +108,7 @@
 //    Console.WriteLine("Max " + B);
 //    Console.WriteLine("Min " + A);
 //}
-//9-misol
+//9 - misol
 //int a = int.Parse(Console.ReadLine());
 //int b = int.Parse(Console.ReadLine());
 //if (a > b)
@@ -118,11 +118,11 @@
 //    a = c;
 //    Console.WriteLine(" b = " + b + " \n a = " + a);
 //}
-//else if (a < b )
+//else if (a < b)
 //{
 //    Console.WriteLine(" b = " + b + " \n a = " + a);
 //}
-//10-misol
+//10 - misol
 //int a = int.Parse(Console.ReadLine());
 //int b = int.Parse(Console.ReadLine());
 //if (a != b)
@@ -133,7 +133,7 @@
 //{
 //    Console.WriteLine(0);
 //}
-//11-misol
+//11 - misol
 //int a = int.Parse(Console.ReadLine());
 //int b = int.Parse(Console.ReadLine());
 //if ((a != b) && (a > b))
@@ -148,7 +148,7 @@
 //{
 //    Console.WriteLine(0);
 //}
-//12-misol
+//12 - misol
 //int a = int.Parse(Console.ReadLine());
 //int b = int.Parse(Console.ReadLine());
 //int c = int.Parse(Console.ReadLine());
@@ -164,11 +164,11 @@
 //{
 //    Console.WriteLine("Kichiki : " + c);
 //}
-//13-misol
+//13 - misol
 //int a = int.Parse(Console.ReadLine());
 //int b = int.Parse(Console.ReadLine());
 //int c = int.Parse(Console.ReadLine());
-//if ((a < b && b < c) || ( c < b && b < a ))
+//if ((a < b && b < c) || (c < b && b < a))
 //{
 //    Console.WriteLine("Natija : " + b);
 //}
@@ -179,9 +179,9 @@
 //else if ((a < c && c < b) || (b < c && c < a))
 //{
 //    Console.WriteLine("Natija : " + c);
-//} 
-//14-misol chiqmadi
-//20 -misol
+//}
+//14 - misol chiqmadi
+//20 - misol
 //int a = int.Parse(Console.ReadLine());
 //int b = int.Parse(Console.ReadLine());
 //int c = int.Parse(Console.ReadLine());
@@ -198,51 +198,51 @@
 //{
 //    Console.WriteLine("yaqin nuqta: " + b + " " + c);
 //}
-//else if (a == b && b == c) 
+//else if (a == b && b == c)
 //{
 //    Console.WriteLine("nuqtalar ustma ust tushadi");
 //}
-// if 24 - misol
+//if 24 - misol
 //double x = double.Parse(Console.ReadLine());
-//if(x > 0)
+//if (x > 0)
 //{
-//    Console.WriteLine("f(x) = " + (2 * (Math.Sin( x))));
+//    Console.WriteLine("f(x) = " + (2 * (Math.Sin(x))));
 //}
-//else if(x <=0)
+//else if (x <= 0)
 //{
 //    Console.WriteLine("f(x) = " + (x - 2));
 //}
-// if 25 - misol
+//if 25 - misol
 //double x = double.Parse(Console.ReadLine());
-//if((x < -2)  (x > 2))
-//        {
+//if ((x < -2)(x > 2))
+//{
 //    Console.WriteLine("f(x) = " + (2 * x));
 //}
 //else
 //{
 //    Console.WriteLine("f(x) = " + (-3 * x));
 //}
-// if 26 - misol
+//if 26 - misol
 //double x = double.Parse(Console.ReadLine());
-//if(x <= 0)
+//if (x <= 0)
 //{
 //    Console.WriteLine("f(x) = " + (-x));
 //}
-//else if((0 < x) && (x < 2))
+//else if ((0 < x) && (x < 2))
 //{
 //    Console.WriteLine("f(x) = " + Math.Pow(2, x));
 //}
-//else if(x >= 2)
+//else if (x >= 2)
 //{
 //    Console.WriteLine("f(x) = " + 4);
 //}
-// if 26 - misol
+//if 26 - misol
 //double x = double.Parse(Console.ReadLine());
-//if(x < 0)
+//if (x < 0)
 //{
 //    Console.WriteLine("f(x) = " + 0);
 //}
-//27-misol
+//27 - misol
 //int x = int.Parse(Console.ReadLine());
 //if (x < 0)
 //{
@@ -256,13 +256,13 @@
 //{
 //    Console.WriteLine(-1);
 //}
-//if 28 -misol
+//if 28 - misol
 //int year = int.Parse(Console.ReadLine());
-//if((year % 4 == 0 && year % 100 != 0)  (year % 400 == 0))
+//if ((year % 4 == 0 && year % 100 != 0)(year % 400 == 0))
 //{
 //    Console.WriteLine("Kabisa yili, bu yilda 366 kun bor");
 //}
-// if 29 -misol
+//if 29 - misol
 //int a = int.Parse(Console.ReadLine());
 //if (a > 0)
 //{
@@ -274,15 +274,17 @@
 //}
 //else if (a == 0)
 //{
-//    Console.WriteLine( 0);
+//    Console.WriteLine(0);
 //}
-//switch Case 5-misol
+//switch Case 5 - misol
 //Console.Write("a = "); double a = double.Parse(Console.ReadLine());
 //char temp = char.Parse(Console.ReadLine());
 //Console.Write("b = "); double b = double.Parse(Console.ReadLine());
 
 //switch (temp)
 //{
+//    default:
+//        Console.WriteLine("bunaqa amal yuq"); break;
 //    case '+':
 //        Console.WriteLine(a + " + " + b + " = " + (a + b));
 //        break;
@@ -295,8 +297,6 @@
 //    case '/':
 //        Console.WriteLine("a / b = " + (a / b));
 //        break;
-//    default:
-//        Console.WriteLine("bunaqa amal yuq"); break;
 //}
 //Case - 7
 //double massaBirligi = double.Parse(Console.ReadLine());
@@ -306,16 +306,16 @@
 //switch (tartibRaqami)
 //{
 //    case 1:
-//        a = massaBirligi * 1;   
+//        a = massaBirligi * 1;
 //        Console.WriteLine(massaBirligi + " kilogram " + a + " kg ");
 //        break;
-//        case 2:
-//        a = massaBirligi / 1000000;  
+//    case 2:
+//        a = massaBirligi / 1000000;
 //        Console.WriteLine(massaBirligi + " milliklagram " + a + " kg ");
 //        break;
 //    case 3:
 //        a = massaBirligi / 1000;
-//        Console.WriteLine(massaBirligi +  "gramm " + a + " kg ");
+//        Console.WriteLine(massaBirligi + "gramm " + a + " kg ");
 //        break;
 //    case 4:
 //        a = massaBirligi * 1000;
@@ -325,8 +325,9 @@
 //        a = massaBirligi * 100;
 //        Console.WriteLine(massaBirligi + " Setner " + a + " kg ");
 //        break;
-//        default: Console.WriteLine(" Bunday ulchov yo'q ");
-//        break;  
+//    default:
+//        Console.WriteLine(" Bunday ulchov yo'q ");
+//        break;
 //}
 
 
